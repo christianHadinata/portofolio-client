4ec4983fe1fa50724bd98e355d26a2757d94bdd2
christianHadinata <christianhadinata2004@gmail.com>
2025-02-16 13:08:27 +0700

    fix: fix scroll breakpoint and set cursor to default on logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Navbar() {
   const [active, setActive] = useState("home");
@@ -13,6 +13,31 @@ export default function Navbar() {
     }
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = ["home", "about", "skills", "portfolio", "contact"];
+      const scrollPosition = window.scrollY + 100; // Adjust offset if needed
+
+      for (const section of sections) {
+        const element = document.getElementById(section);
+        if (element) {
+          const offsetTop = element.offsetTop;
+          const offsetBottom = offsetTop + element.offsetHeight;
+
+          if (scrollPosition >= offsetTop && scrollPosition < offsetBottom) {
+            setActive(section);
+            break;
+          }
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   const links = [
     { name: "Home", id: "home" },
     { name: "About", id: "about" },
@@ -24,7 +49,9 @@ export default function Navbar() {
   return (
     <nav className="fixed z-50 w-full bg-blue-950 px-36 py-6 text-white">
       <div className="flex items-center justify-between px-8">
-        <h1 className="text-2xl font-medium capitalize">Chris Portfolio</h1>
+        <h1 className="cursor-default text-2xl font-medium capitalize">
+          Chris Portfolio
+        </h1>
         <div className="flex gap-x-10">
           {links.map((link) => (
             <button
